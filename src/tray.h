b381d14aea3025d40908d6e3297f97111d739f89
csy214-beep <csy2146047622@gmail.com>
2026-05-17 00:36:49 +0800

    build: vertion number

--- src/tray.h
+++ src/tray.h
@@ -10,7 +10,7 @@ class TrayIcon : public QSystemTrayIcon {
     Q_OBJECT
 
 public:
-    QString Version = "ver20260501.7";
+    QString Version = "ver20260517.8";
 
     TrayIcon(const TrayIcon &) = delete;
     TrayIcon &operator=(const TrayIcon &) = delete;
